de5ad585aa8eb993dce2f1a6684468c6706539b2
helpfulrobot <chris@silverstripe.com>
2016-01-01 02:30:57 +1300

    Converted to PSR-2

--- code/dataobjects/ContactInquiry.php
+++ code/dataobjects/ContactInquiry.php
@@ -1,118 +1,129 @@
 <?php
 
-class ContactInquiry Extends DataObject {
+class ContactInquiry extends DataObject
+{
 
-	private static $db = array(
-		"FirstName" => "Varchar(255)",
-		"LastName" => "Varchar(255)",
-		"Email" => "Varchar(255)",
-		"Phone" => "Varchar(255)",
-		"Locale" => "Varchar(255)",
-		"Ref" => "Varchar(255)",
-		"Description" => "Text",
-		"AdminComment" => "Text",
-		'Status' => "Enum('New, Opened, Answered, Spam, Archived', 'New')",
-		"Sort" => "Int"
-	);
+    private static $db = array(
+        "FirstName" => "Varchar(255)",
+        "LastName" => "Varchar(255)",
+        "Email" => "Varchar(255)",
+        "Phone" => "Varchar(255)",
+        "Locale" => "Varchar(255)",
+        "Ref" => "Varchar(255)",
+        "Description" => "Text",
+        "AdminComment" => "Text",
+        'Status' => "Enum('New, Opened, Answered, Spam, Archived', 'New')",
+        "Sort" => "Int"
+    );
 
-	private static $has_one = array(
-	);
+    private static $has_one = array(
+    );
 
-	private static $casting = array(
-		"Title" => "Varchar(255)"
-	);
+    private static $casting = array(
+        "Title" => "Varchar(255)"
+    );
 
-	private static $defaults = array(
-		'Status' => 'New'
-	);
+    private static $defaults = array(
+        'Status' => 'New'
+    );
 
-	private static $singular_name = 'Contact Inquiry';
-	private static $plural_name = 'Contact Inquiries';
-	private static $default_sort = 'Sort, ID Desc';
+    private static $singular_name = 'Contact Inquiry';
+    private static $plural_name = 'Contact Inquiries';
+    private static $default_sort = 'Sort, ID Desc';
 
-	public static $searchable_fields = array(
-		"FirstName",
-		"LastName",
-		"Email",
-		"Phone",
-		"Status"
-	);
+    public static $searchable_fields = array(
+        "FirstName",
+        "LastName",
+        "Email",
+        "Phone",
+        "Status"
+    );
 
-	public static $summary_fields = array(
-		"FullName",
-		"Email",
-		"Status",
-		"Locale",
-		"Created"
-	);
+    public static $summary_fields = array(
+        "FullName",
+        "Email",
+        "Status",
+        "Locale",
+        "Created"
+    );
 
-	public static $field_labels = array(
-		"FullName" => "Full Name",
-		"Sort" => "Sort Index",
-	);
+    public static $field_labels = array(
+        "FullName" => "Full Name",
+        "Sort" => "Sort Index",
+    );
 
-	public function getFullName() {
-		return $this->FirstName .' '. $this->LastName;
-	}
+    public function getFullName()
+    {
+        return $this->FirstName .' '. $this->LastName;
+    }
 
-	public function FullName() {
-		return $this->getFullName();
-	}
+    public function FullName()
+    {
+        return $this->getFullName();
+    }
 
-	public function getTitle() {
-		return $this->getFullName() .' / '. $this->Status .' / '. $this->Created;
-	}
+    public function getTitle()
+    {
+        return $this->getFullName() .' / '. $this->Status .' / '. $this->Created;
+    }
 
-	public function Title() {
-		return $this->getTitle();
-	}
+    public function Title()
+    {
+        return $this->getTitle();
+    }
 
-	public static function get_contactinquiry_status_options() {
-		return singleton('ContactInquiry')->dbObject('Status')->enumValues(false);
-	}
+    public static function get_contactinquiry_status_options()
+    {
+        return singleton('ContactInquiry')->dbObject('Status')->enumValues(false);
+    }
 
-	public function getCMSFields(){
-		$fields = new FieldList(new TabSet('Root', new Tab('Main')));
-		$fields->removeByName("Sort");
+    public function getCMSFields()
+    {
+        $fields = new FieldList(new TabSet('Root', new Tab('Main')));
+        $fields->removeByName("Sort");
 
-		$dropFieldStatus = new DropdownField('Status', 'Status', self::get_contactinquiry_status_options());
+        $dropFieldStatus = new DropdownField('Status', 'Status', self::get_contactinquiry_status_options());
 
-		$tabName = singleton("ContactInquiry")->singular_name();
-		$fields->addFieldsToTab('Root.Main', array(
-			new HeaderField("$tabName details"),
-			$dropFieldStatus,
-			new ReadOnlyField('FirstName', 'First Name'),
-			new ReadOnlyField('LastName', 'Last Name'),
-			new ReadOnlyField('Email', 'Email'),
-			new ReadOnlyField('Phone', 'Phone'),
-			new ReadOnlyField('Ref', 'Referal'),
-			new ReadOnlyField('Locale', 'Locale'),
-			new ReadOnlyField('Created', 'Created'),
-			new ReadOnlyField('Description', 'Description'),
-			new TextareaField('AdminComment', 'Admin Comment')
-		));
+        $tabName = singleton("ContactInquiry")->singular_name();
+        $fields->addFieldsToTab('Root.Main', array(
+            new HeaderField("$tabName details"),
+            $dropFieldStatus,
+            new ReadOnlyField('FirstName', 'First Name'),
+            new ReadOnlyField('LastName', 'Last Name'),
+            new ReadOnlyField('Email', 'Email'),
+            new ReadOnlyField('Phone', 'Phone'),
+            new ReadOnlyField('Ref', 'Referal'),
+            new ReadOnlyField('Locale', 'Locale'),
+            new ReadOnlyField('Created', 'Created'),
+            new ReadOnlyField('Description', 'Description'),
+            new TextareaField('AdminComment', 'Admin Comment')
+        ));
 
-		return $fields;
-	}
+        return $fields;
+    }
 
-	function onBeforeWrite() {
-		parent::onBeforeWrite();
-	}
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+    }
 
-	public function canView($member = null) {
-		return true;
-	}
+    public function canView($member = null)
+    {
+        return true;
+    }
 
-	public function canCreate($member = null) {
-		return true;
-	}
+    public function canCreate($member = null)
+    {
+        return true;
+    }
 
-	public function canEdit($member = null) {
-		return true;
-	}
+    public function canEdit($member = null)
+    {
+        return true;
+    }
 
-	public function canDelete($member = null) {
-		return true;
-	}
-
+    public function canDelete($member = null)
+    {
+        return true;
+    }
 }
--- code/forms/ContactInquiryForm.php
+++ code/forms/ContactInquiryForm.php
@@ -1,137 +1,145 @@
 <?php
 
-class ContactInquiryForm extends BootstrapForm {
+class ContactInquiryForm extends BootstrapForm
+{
 
-	protected $currController = false;
+    protected $currController = false;
 
-	function __construct(Controller $controller, $name) {
-		$this->currController = $controller;
+    public function __construct(Controller $controller, $name)
+    {
+        $this->currController = $controller;
 
-		$fields = new FieldList(
-			array(
-				TextField::create("FirstName")
-					->setTitle(_t("ContactPage.FIRSTNAME", "First Name"))
-					->addPlaceholder(_t("ContactPage.FIRSTNAMEPLACEHOLDER", "Your First Name"))
-					->setMaxLength(55)
-					->setAttribute("autocomplete", "off"),
+        $fields = new FieldList(
+            array(
+                TextField::create("FirstName")
+                    ->setTitle(_t("ContactPage.FIRSTNAME", "First Name"))
+                    ->addPlaceholder(_t("ContactPage.FIRSTNAMEPLACEHOLDER", "Your First Name"))
+                    ->setMaxLength(55)
+                    ->setAttribute("autocomplete", "off"),
 
-				TextField::create("LastName")
-					->setTitle(_t("ContactPage.LASTNAME", "Last Name"))
-					->addPlaceholder(_t("ContactPage.LASTNAMEPLACEHOLDER", "Your Last Name"))
-					->setMaxLength(55)
-					->setAttribute("autocomplete", "off"),
+                TextField::create("LastName")
+                    ->setTitle(_t("ContactPage.LASTNAME", "Last Name"))
+                    ->addPlaceholder(_t("ContactPage.LASTNAMEPLACEHOLDER", "Your Last Name"))
+                    ->setMaxLength(55)
+                    ->setAttribute("autocomplete", "off"),
 
-				EmailField::create("Email")
-					->setTitle(_t("ContactPage.EMAIL", "Email"))
-					->addPlaceholder(_t("ContactPage.EMAILPLACEHOLDER", "Your Email address"))
-					->setMaxLength(55)
-					->setAttribute("autocomplete", "off"),
+                EmailField::create("Email")
+                    ->setTitle(_t("ContactPage.EMAIL", "Email"))
+                    ->addPlaceholder(_t("ContactPage.EMAILPLACEHOLDER", "Your Email address"))
+                    ->setMaxLength(55)
+                    ->setAttribute("autocomplete", "off"),
 
-				TextField::create("Phone")
-					->setTitle(_t("ContactPage.PHONE", "Phone"))
-					->addPlaceholder(_t("ContactPage.PHONEPLACEHOLDER", "Your Phone number"))
-					->setMaxLength(55)
-					->setAttribute("autocomplete", "off"),
+                TextField::create("Phone")
+                    ->setTitle(_t("ContactPage.PHONE", "Phone"))
+                    ->addPlaceholder(_t("ContactPage.PHONEPLACEHOLDER", "Your Phone number"))
+                    ->setMaxLength(55)
+                    ->setAttribute("autocomplete", "off"),
 
-				TextareaField::create("Description")
-					->setTitle(_t("ContactPage.DESCRIPTION", "Message"))
-					->addPlaceholder(_t("ContactPage.DESCRIPTIONPLACEHOLDER", "Your Message for us"))
-					->setRows(6),
+                TextareaField::create("Description")
+                    ->setTitle(_t("ContactPage.DESCRIPTION", "Message"))
+                    ->addPlaceholder(_t("ContactPage.DESCRIPTIONPLACEHOLDER", "Your Message for us"))
+                    ->setRows(6),
 
-				TextField::create("Url")
-					->setTitle("")
-					->addPlaceholder(_t("ContactPage.URL", "Url"))
-					->setMaxLength(55)
-					->setAttribute("autocomplete", "off"),
+                TextField::create("Url")
+                    ->setTitle("")
+                    ->addPlaceholder(_t("ContactPage.URL", "Url"))
+                    ->setMaxLength(55)
+                    ->setAttribute("autocomplete", "off"),
 
-				TextareaField::create("Comment")
-					->setTitle("")
-					->addPlaceholder(_t("ContactPage.COMMENT", "Comment"))
-					->setRows(6),
+                TextareaField::create("Comment")
+                    ->setTitle("")
+                    ->addPlaceholder(_t("ContactPage.COMMENT", "Comment"))
+                    ->setRows(6),
 
-				HiddenField::create("Ref")->setValue($controller->Title),
-				HiddenField::create("Locale")->setValue($controller->Locale)
-			)
-		);
+                HiddenField::create("Ref")->setValue($controller->Title),
+                HiddenField::create("Locale")->setValue($controller->Locale)
+            )
+        );
 
-		$actions = new FieldList(
-			FormAction::create('dosave', _t("ContactPage.APPLY","Send"))->setStyle("success")
-		);
+        $actions = new FieldList(
+            FormAction::create('dosave', _t("ContactPage.APPLY", "Send"))->setStyle("success")
+        );
 
-		$validator = ZenValidator::create();
-		$validator->addRequiredFields(array('FirstName', 'LastName', 'Email', 'Description'));
-		$validator->setConstraint('Email', Constraint_type::create('email'));
-		$validator->setConstraint('Description', Constraint_length::create('min', 10));
+        $validator = ZenValidator::create();
+        $validator->addRequiredFields(array('FirstName', 'LastName', 'Email', 'Description'));
+        $validator->setConstraint('Email', Constraint_type::create('email'));
+        $validator->setConstraint('Description', Constraint_length::create('min', 10));
 
-		parent::__construct($controller, $name, $fields, $actions, $validator);
+        parent::__construct($controller, $name, $fields, $actions, $validator);
 
-		if($this->extend('updateFields', $fields) !== null) {$this->setFields($fields);}
-		if($this->extend('updateActions', $actions) !== null) {$this->setActions($actions);}
-		// if($this->extend('updateValidator', $requiredFields) !== null) {$this->setValidator($requiredFields);}
+        if ($this->extend('updateFields', $fields) !== null) {
+            $this->setFields($fields);
+        }
+        if ($this->extend('updateActions', $actions) !== null) {
+            $this->setActions($actions);
+        }
+        // if($this->extend('updateValidator', $requiredFields) !== null) {$this->setValidator($requiredFields);}
 
-		$oldData = Session::get("FormInfo.{$this->FormName()}.data");
-		if($oldData && (is_array($oldData) || is_object($oldData))) {
-			$this->loadDataFrom($oldData);
-		}
-		$this->extend('updateContactInquiryForm', $this);
-	}
+        $oldData = Session::get("FormInfo.{$this->FormName()}.data");
+        if ($oldData && (is_array($oldData) || is_object($oldData))) {
+            $this->loadDataFrom($oldData);
+        }
+        $this->extend('updateContactInquiryForm', $this);
+    }
 
-	/**
-	 * Form action handler for ContactInquiryForm.
-	 *
-	 * @param array $data The form request data submitted
-	 * @param Form $form The {@link Form} this was submitted on
-	 */
-	function dosave(Array $data, Form $form, SS_HTTPRequest $request) {
-		$SQLData = Convert::raw2sql($data);
-		$attrs = $form->getAttributes();
+    /**
+     * Form action handler for ContactInquiryForm.
+     *
+     * @param array $data The form request data submitted
+     * @param Form $form The {@link Form} this was submitted on
+     */
+    public function dosave(array $data, Form $form, SS_HTTPRequest $request)
+    {
+        $SQLData = Convert::raw2sql($data);
+        $attrs = $form->getAttributes();
 
-		if($SQLData['Comment'] != '' || $SQLData['Url'] != '' ){
-			// most probably spam - terminate silently
-			Director::redirect(Director::baseURL(). $this->URLSegment . "/success");
-			return;
-		}
+        if ($SQLData['Comment'] != '' || $SQLData['Url'] != '') {
+            // most probably spam - terminate silently
+            Director::redirect(Director::baseURL(). $this->URLSegment . "/success");
+            return;
+        }
 
-		$item = new ContactInquiry();
-		$form->saveInto($item);
+        $item = new ContactInquiry();
+        $form->saveInto($item);
         // $form->sessionMessage(_t("ContactPage.FORMMESSAGEGOOD", "Your inquiry has been submitted. Thanks!"), 'good');
-		$item->write();
+        $item->write();
 
-		$mailFrom =  $this->currController->MailFrom ? $this->currController->MailFrom : $SQLData['Email'];
-		$mailTo = $this->currController->MailTo ? $this->currController->MailTo : Email::getAdminEmail();
-		$mailSubject = $this->currController->MailSubject ? ($this->currController->MailSubject  .' - '. $SQLData['Ref']) : _t('ContactPage.SUBJECT', '[web] New contact inquiry - ') .' '. $data['Ref'];
+        $mailFrom =  $this->currController->MailFrom ? $this->currController->MailFrom : $SQLData['Email'];
+        $mailTo = $this->currController->MailTo ? $this->currController->MailTo : Email::getAdminEmail();
+        $mailSubject = $this->currController->MailSubject ? ($this->currController->MailSubject  .' - '. $SQLData['Ref']) : _t('ContactPage.SUBJECT', '[web] New contact inquiry - ') .' '. $data['Ref'];
 
-		$email = new Email($mailFrom, $mailTo, $mailSubject);
-		$email->replyTo($SQLData['Email']);
-		$email->setTemplate("ContactInquiry");
-		$email->populateTemplate($SQLData);
-		$email->send();
+        $email = new Email($mailFrom, $mailTo, $mailSubject);
+        $email->replyTo($SQLData['Email']);
+        $email->setTemplate("ContactInquiry");
+        $email->populateTemplate($SQLData);
+        $email->send();
 
-		// $this->controller->redirectBack();
-		if($email->send()){
-			$this->controller->redirect($this->controller->Link() . "success");
-		} else {
-			$this->controller->redirect($this->controller->Link() . "error");
-		}
-		return false;
-	}
+        // $this->controller->redirectBack();
+        if ($email->send()) {
+            $this->controller->redirect($this->controller->Link() . "success");
+        } else {
+            $this->controller->redirect($this->controller->Link() . "error");
+        }
+        return false;
+    }
 
-	/**
-	 * saves the form into session
-	 * @param Array $data - data from form.
-	 */
-	public function saveDataToSession(){
-		$data = $this->getData();
-		Session::set("FormInfo.{$this->FormName()}.data", $data);
-	}
-
+    /**
+     * saves the form into session
+     * @param Array $data - data from form.
+     */
+    public function saveDataToSession()
+    {
+        $data = $this->getData();
+        Session::set("FormInfo.{$this->FormName()}.data", $data);
+    }
 }
 
-class ContactInquiryForm_Validator extends RequiredFields {
+class ContactInquiryForm_Validator extends RequiredFields
+{
 
-	function php($data){
-		$this->form->saveDataToSession();
-		return parent::validate($data);
-	}
-
+    public function php($data)
+    {
+        $this->form->saveDataToSession();
+        return parent::validate($data);
+    }
 }
