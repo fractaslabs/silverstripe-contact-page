21ced9c65781df1941088be9a259c7d68a331806
Petar Simic <petar.simic@fractas.com>
2018-01-27 18:27:40 +0100

    added an ModelAdmin font-icon

--- src/ContactInquiryAdmin.php
+++ src/ContactInquiryAdmin.php
@@ -7,13 +7,16 @@ use SilverStripe\Admin\ModelAdmin;
 class ContactInquiryAdmin extends ModelAdmin
 {
     public $showImportForm = false;
-    
+
     private static $managed_models = [
         ContactInquiry::class,
     ];
 
     private static $url_segment = 'contactinquiry';
+
     private static $menu_title = 'Contact Inq.';
+
+    private static $menu_icon = 'font-icon-book';
 
     public function init()
     {
